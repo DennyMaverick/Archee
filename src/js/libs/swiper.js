517630eec6e663c8e92368116ada2dc3918f5ef8
Denny Maverick <den_maverick177@mail.ru>
2022-02-21 14:33:48 +0300

    Слайдер - доработка

--- src/js/libs/swiper.js
+++ src/js/libs/swiper.js
@@ -4,9 +4,18 @@ import Swiper, { Navigation } from 'swiper';
 const swiper = new Swiper('.swiper', {
   modules: [Navigation],
   loop: true,
-  slidesPerView: 3,
+  slidesPerView: 1,
   navigation: {
     nextEl: '.arrow-next',
     prevEl: '.arrow-prev',
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+      width: 1140
+    }
   }
 });
