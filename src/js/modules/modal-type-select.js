228b129d80fc56bd58d0283af94af574dff12d6d
Denny Maverick <den_maverick177@mail.ru>
2022-05-10 08:55:23 +0300

    Исправление ошибки: при клике на ссылки значение hash в строке браузера менялось и это некорректно работало с выбором типов работ в модальном окне modal-type, теперь все работает корректно

--- src/js/modules/modal-type-select.js
+++ src/js/modules/modal-type-select.js
@@ -21,22 +21,41 @@ const houseItems = document.querySelectorAll(".house-item")
 const modalTabs = document.querySelectorAll(".modal-type__text")
 const modalTypeBtn = document.querySelector(".modal-type__btn")
 const searchInputType = document.querySelector(".search__item--type")
+const langName = document.querySelector(".lang__name")
+const modalTypeInner = document.querySelector(".modal-type__inner")
+const modalType = document.querySelector(".modal-type")
+const bodyEl = document.querySelector("body")
+
+//* При клике на кнопку 'Принять' в модальном окне modal-type происходит закрытие модального окна с отображением значения выбранного типа работ в поле input в секции === search ===
+
+//* При смене языка все работает корректно, когда hash в строке браузера изменен на якорь ссылки, значение типа работ в поле input отображается корректно
 
 modalTypeBtn.addEventListener("click", (event) => {
   const modalContentInner = event.target.closest(".modal__inner")
-  const modalTypeInner = document.querySelector(".modal-type__inner")
-  const modalType = document.querySelector(".modal-type")
-  const bodyEl = document.querySelector("body")
+
+  const allLangs = ["ru", "en"]
 
   const currentTexts = modalContentInner.querySelectorAll(".modal-type__text")
   currentTexts.forEach((item) => {
     if (!item.classList.contains("hide")) {
+      let currentLang = langName.textContent
+
       const itemId = item.id
-      let hash = window.location.hash
-      hash = hash.substr(1)
+
       const currentKey = `modal-type-${itemId}`
-      if (searchInputType && cityTypes[currentKey][hash]) {
-        searchInputType.value = cityTypes[currentKey][hash]
+
+      let hash = window.location.hash
+
+      hash = hash.substr(1)
+
+      if (!allLangs.includes(hash)) {
+        if (searchInputType && cityTypes[currentKey][currentLang]) {
+          searchInputType.value = cityTypes[currentKey][currentLang]
+        }
+      } else {
+        if (searchInputType && cityTypes[currentKey][hash]) {
+          searchInputType.value = cityTypes[currentKey][hash]
+        }
       }
     }
   })
@@ -48,6 +67,8 @@ modalTypeBtn.addEventListener("click", (event) => {
     bodyEl.classList.remove("no-scroll")
   }, 500)
 })
+
+//* Плавный скролл вниз при клике на иконки типов работ
 
 houseItems.forEach((item) => {
   item.addEventListener("click", function () {
