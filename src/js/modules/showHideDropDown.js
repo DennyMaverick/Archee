8c2f9bd2e91ffd27dce9e041bad685d705b88a17
Denny Maverick <den_maverick177@mail.ru>
2022-08-20 11:39:04 +0300

    Исправление мелких ошибок: добавление возможности переходить на вкладку домой + увеличение кликабельности кнопки изменения языков + исправление ошибки в js

--- src/js/modules/showHideDropDown.js
+++ src/js/modules/showHideDropDown.js
@@ -5,9 +5,7 @@ const arrowsBtn = document.querySelectorAll(".lang__arrow-btn")
 arrowsBtn.forEach(function (arrow) {
   arrow.addEventListener("click", function () {
     const currentLangList = this.nextElementSibling
-
     currentLangList.classList.toggle("lang-list__show")
-
     this.querySelector(".lang__arrow").classList.toggle("lang__arrow-rotate")
   })
 })
@@ -16,7 +14,7 @@ window.addEventListener("click", (e) => {
   const langLists = document.querySelectorAll(".lang-list")
   langLists.forEach((item) => {
     if (item.classList.contains("lang-list__show")) {
-      if (!e.target.classList.contains("lang__arrow")) {
+      if (!e.target.classList.contains("lang__arrow-btn")) {
         langLists.forEach((item) => {
           item.classList.remove("lang-list__show")
         })
